53781e5a64589ac40075bc2b664435a3dfc22e9c
V S Bharath <bharathcse5t6@gmail.com>
2018-10-16 13:06:29 +0530

    cross-origin-resource-sharing

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(morgan('dev'))
 app.use(bodyparser.urlencoded({extended : false}))
 app.use(bodyparser.json())
 
+//cors
+app.use((req,res,next)=> {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization')
+})
+if(req.method === "OPTIONS") {
+    res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET')
+}
+
+
 app.use('/orders',orderRoutes)
 app.use('/products',productRoutes)
 
