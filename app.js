5d68ca129f75e95bb38e96285b0873855bcc3c0f
V S Bharath <bharathcse5t6@gmail.com>
2018-10-16 12:29:41 +0530

    handling global errors

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ app.use(morgan('dev'))
 app.use('/orders',orderRoutes)
 app.use('/products',productRoutes)
 
+app.use((req,res,next) => {
+      const error = new Error('not found')
+      error.status = 404
+      //this will forward our customized request instead of
+      //original request
+      next(error)
+})
+app.use((error,req,res,next)=> {
+    //all the errors
+    res.status(error.status || 500)
+    res.json({
+        error : {
+            message : error.message
+        }
+    })
+})
+
 app.use((req,res)=>{
     res.status(200).json({
         message : "working"
