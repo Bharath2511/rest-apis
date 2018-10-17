3506922fc739a50ef64a110703ef97d924db3e56
V S Bharath <bharathcse5t6@gmail.com>
2018-10-17 17:33:43 +0530

    required bcrypt

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 
+const User = require('../models/user')
+
+router.post('/signup',(req,res,nex)=> {
+   const user = new User({
+       _id : new mongoose.Types.ObjectId(),
+       email : req.body.email,
+       password : 
+   })
+})
 
 module.exports = router
