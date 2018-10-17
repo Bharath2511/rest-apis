cad69a5c08e5374205c74522961bacb62da50dc5
V S Bharath <bharathcse5t6@gmail.com>
2018-10-17 10:58:50 +0530

    modified index route for orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,8 +5,30 @@ const Order = require('../models/order')
 const Product = require('../models/product')
 
 router.get('/',(req,res,next)=>{
-     res.status(200).json({
-        message : "orders are fetched" 
+     Order.find()
+     .select('product quantity _id')
+     .exec()
+     .then(docs => {
+         res.status(200).json({
+             count : docs.length,
+             orders : docs.map(doc => {
+                 return {
+                     _id : doc._id,
+                     product : doc.product,
+                     quantity : doc.quantity,
+                     request : {
+                        type : "GET",
+                        url : "http://localhost:3000/orders/" +doc._id
+                    }
+                 }
+             }),
+             
+         })
+     })
+     .catch(err => {
+         res.status(500).json({
+             error : err
+         })
      })
 })
 
