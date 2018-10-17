3eb10d764e0af540d5822fb5fa1f51943a337786
V S Bharath <bharathcse5t6@gmail.com>
2018-10-17 18:09:04 +0530

    deleting users

--- api/routes/users.js
+++ api/routes/users.js
@@ -49,4 +49,20 @@ router.post('/signup',(req,res,nex)=> {
    
 })
 
+router.delete('/:userId',(req,res,next)=> {
+    User.remove({_id : req.params.userId}).exec()
+    .then(result => {
+        res.status(200).json({
+            message : "user deleted"
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        return res.status(500).json({
+            error : err
+        
+    })
+})
+})
+
 module.exports = router
